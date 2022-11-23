da3d6a6cc6fa1ae2c63c76f5628ee53756762362
Dani <dagugo.96@gmail.com>
2022-11-23 16:05:38 -0500

    Project pages iteration finished with test data

---
+++ src/helpers/ProjectList.js
@@ -0,0 +1,16 @@
+import TestImg from "../assets/HiRes-17.jpg";
+
+export const ProjectList = [
+  {
+    name: "Ability Base",
+    image: TestImg,
+  },
+  {
+    name: "Samsong",
+    image: TestImg,
+  },
+  {
+    name: "Dawn Of Fire",
+    image: TestImg,
+  },
+];
