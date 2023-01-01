9f0b983fe16281e99a2eaed3ffbac9ab4f979c8d
Danilo Vanegas <dagugo.96@gmail.com>
2023-01-01 12:43:05 -0500

    some links fixed

--- src/components/ContactIcons.js
+++ src/components/ContactIcons.js
@@ -2,22 +2,32 @@ import React from "react";
 import InstagramIcon from "@mui/icons-material/Instagram";
 import TwitterIcon from "@mui/icons-material/Twitter";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import EmailIcon from "@mui/icons-material/Email";
 import "../styles/ContactIcons.css";
 
 function ContactIcons() {
   return (
     <div className="socialMedia">
-      <InstagramIcon
-        onClick={() =>
-          window.open("https://www.instagram.com/_dani.svs/", "_blank")
-        }
-      />
-      <TwitterIcon />
-      <LinkedInIcon
-        onClick={() =>
-          window.open("https://www.linkedin.com/in/danisvs/", "_blank")
-        }
-      />
+      <a href="mailto:hello@kemiadeleke.com" target="_blank" rel="noreferrer">
+        <EmailIcon />
+      </a>
+      <a
+        href="https://www.instagram.com/_dani.svs/"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <InstagramIcon />
+      </a>
+      <a>
+        <TwitterIcon />
+      </a>
+      <a
+        href="https://www.linkedin.com/in/danisvs/"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <LinkedInIcon />
+      </a>
     </div>
   );
 }
