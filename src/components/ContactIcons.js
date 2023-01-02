25c4569dda529d2c0f67bbc39a05516fb6b7c29d
Danilo Vanegas <dagugo.96@gmail.com>
2023-01-02 16:23:51 -0500

    contact icons minor changes

--- src/components/ContactIcons.js
+++ src/components/ContactIcons.js
@@ -1,6 +1,7 @@
 import React from "react";
 import InstagramIcon from "@mui/icons-material/Instagram";
 import TwitterIcon from "@mui/icons-material/Twitter";
+import GitHubIcon from "@mui/icons-material/GitHub";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
 import EmailIcon from "@mui/icons-material/Email";
 import "../styles/ContactIcons.css";
@@ -18,8 +19,8 @@ function ContactIcons() {
       >
         <InstagramIcon />
       </a>
-      <a>
-        <TwitterIcon />
+      <a href="https://github.com/Playyer96" target="_blank" rel="noreferrer">
+        <GitHubIcon />
       </a>
       <a
         href="https://www.linkedin.com/in/danisvs/"
